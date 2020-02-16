dd8f4d9cb1a113c0b1d6753a228b3a4d691519ed
Alessia <miccoli.alessia@hotmail.it>
2020-02-16 10:33:42 +0100

    changed favicon

--- magic-ball/src/App.js
+++ magic-ball/src/App.js
@@ -11,8 +11,8 @@ function App() {
       <MagicBall/>
       <footer>
         <div className="copyright">
-          <p>Created By Alessia Miccoli -     
-          <a target="_blank" rel="noopener noreferrer" href="https://icons8.com/icons/set/fantasy"> Fantasy icon</a> icon by <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">Icons8</a></p>
+          <p>Created By Alessia Miccoli</p>    
+          <p><a target="_blank" rel="noopener noreferrer" href="https://icons8.com/icons/set/fantasy"> Fantasy icon</a> icon by <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">Icons8</a></p>
         </div>
       </footer>
     </div>
