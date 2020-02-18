58f9ef9f0aa08e77c903fbee9ef26fece26358d6
Alessia <miccoli.alessia@hotmail.it>
2020-02-18 09:15:35 +0100

    Updates

---
+++ precache-manifest.71e3bc4685e29c53cf589c0252006a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466cbbfe579c169a956c95f7c9af70d6",
+    "url": "/magic-ball/index.html"
+  },
+  {
+    "revision": "3a7d839a824b2468963a",
+    "url": "/magic-ball/static/css/main.0125b346.chunk.css"
+  },
+  {
+    "revision": "c23e2bb043aa2ebdbfb2",
+    "url": "/magic-ball/static/js/2.d7bcd129.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/magic-ball/static/js/2.d7bcd129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7d839a824b2468963a",
+    "url": "/magic-ball/static/js/main.b3c34f7c.chunk.js"
+  },
+  {
+    "revision": "250b7f005958e4e5a6ae",
+    "url": "/magic-ball/static/js/runtime-main.5fe5d46d.js"
+  }
+]);
